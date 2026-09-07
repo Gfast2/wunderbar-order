ca1444a1fbcc85ef13f456e8da171af286fb491a
Su Gao <gfast2@gmail.com>
2026-09-07 12:03:19 +0200

    feat:customization / touch up in page header

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -163,8 +163,7 @@ function Header() {
     <header className="relative border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
         <Link href="/" className="flex items-center">
-          <CircleIcon className="h-6 w-6 text-orange-500" />
-          <span className="ml-2 text-xl font-semibold text-gray-900">Wunderbar Order</span>
+          <img src="logo-text.png" alt="Wunderbar Logo" className="h-7" />
         </Link>
         <div className="flex items-center space-x-4">
           <button
